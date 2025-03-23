0bb947ca2adfb42ba7778f648ecd41caf4cea66e
ArunRoy404 <arunroy0065@gmail.com>
2025-03-23 15:38:10 +0600

    props dfault value tested

--- introduction-to-react/src/App.jsx
+++ introduction-to-react/src/App.jsx
@@ -1,17 +1,53 @@
 import { useState } from 'react'
 import './App.css'
 
+const defaultStyle = {
+  margin: '10px',
+  border: '2px solid cyan',
+  borderRadius: '20px',
+  padding: '20px'
+}
+
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>React Basic Components</h1>
+      {/* <Person></Person>
       <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person></Person> */}
 
       <Developer name="x" tech="c++"></Developer>
       <Developer name='y' tech='python'></Developer>
+
+      <Phone brand='apple' price='100000' ></Phone>
+      <Player game='football' name='neymar' ></Player>
+
+      <Salami event='eid' amount='500'></Salami>
+      <Salami event='eid'></Salami>
     </>
+  )
+}
+
+function Salami({ event, amount = 'no taka no salam' }) {
+  return (
+    <div style={{
+      border: '2px solid blue',
+      margin: '10px',
+      borderRadius: '20px'
+    }}>
+      <p>Event: {event}</p>
+      <p>Money: {amount}</p>
+    </div>
+  )
+}
+
+
+function Player({ game, name }) {
+  return (
+    <div style={defaultStyle}>
+      <p>Game: {game}</p>
+      <p>Player Name: {name}</p>
+    </div>
   )
 }
 
@@ -28,17 +64,30 @@ function Person() {
 }
 
 function Developer(props) {
-
   const devStyle = {
     border: '2px solid green',
     margin: '10px',
     borderRadius: '20px'
   }
-
   return (
     <div style={devStyle}>
       <h3>Name: {props.name}</h3>
       <h3>Tech: {props.tech}</h3>
+    </div>
+  )
+}
+
+function Phone(props) {
+  const phone = {
+    border: '2px solid purple',
+    margin: '10px',
+    padding: '10px',
+    borderRadius: '40px'
+  }
+  return (
+    <div style={phone}>
+      <p>brand: {props.brand}</p>
+      <p>price: {props.price}</p>
     </div>
   )
 }
