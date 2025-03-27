303c724ee5ef4853aa39e645e20eea4676a4d6cc
ArunRoy404 <arunroy0065@gmail.com>
2025-03-28 00:09:17 +0600

    create element using bable

--- understand-react/scratch-react/react.js
+++ understand-react/scratch-react/react.js
@@ -28,8 +28,39 @@ const parentElement = React.createElement(
 
 // render the element 
 // ReactDOM.createRoot(document.getElementById('root')).render(myElement)
-ReactDOM.createRoot(document.getElementById('root')).render(parentElement)
+// ReactDOM.createRoot(document.getElementById('root')).render(parentElement)
 
 
-console.log(childElement)
-console.log(parentElement)
+// console.log(childElement)
+// console.log(parentElement)
+
+
+//Browsers don't recognize this format
+// this is not jsx Format 
+// so need parser to convert it to jsx
+// use bable
+const newElement = (
+    <div>
+        <p>This is from jsx type</p>
+    </div>
+)
+
+// can see in console that the element has been converted into jsx format 
+console.log(newElement)
+// ReactDOM.createRoot(document.getElementById('root')).render(newElement)
+
+const NewElementFN = () =>{
+    return (
+        <div>
+            <p>This element is from non jsx function</p>
+        </div>
+    )
+}
+
+// console.log(newElementFN())
+// ReactDOM.createRoot(document.getElementById('root')).render(NewElementFN())
+
+
+// should use capital at first for component 
+console.log(<NewElementFN></NewElementFN>)
+ReactDOM.createRoot(document.getElementById('root')).render(<NewElementFN></NewElementFN>)
