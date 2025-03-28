7fd02e510e1e98a55b20f379027edd4f70989976
ArunRoy404 <arunroy0065@gmail.com>
2025-03-28 14:22:01 +0600

    loading added in useEffect

--- understand-react/src/Components/Products/Products.jsx
+++ understand-react/src/Components/Products/Products.jsx
@@ -6,14 +6,28 @@ const Products = () => {
 
     const [products, setProducts] = useState([])
 
+    // loading and error handler
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState('')
+
     useEffect(() => {
-        fetch(url).then(response => response.json()).then(data => setProducts(data))
+        setIsLoading(true);
+        console.log(isLoading)
+        fetch(url)
+        .then(response => response.json())
+        .then(data =>{
+            setProducts(data)
+            setIsLoading(false)
+        })
     }, [])
 
     return (
         <div className="border-2 border-black mt-6 bg-gray-200 p-5">
             <h2 className="text-3xl">Total Products: {products.length}</h2>
             <ul className="list-decimal p-10">
+
+                {/* conditional rendering */}
+                {isLoading && <h2 className="text-2xl font-bold">Loading Products..........</h2>}
                 {
                     products.map(product => <Product key={product.id} product={product}></Product>)
                 }
