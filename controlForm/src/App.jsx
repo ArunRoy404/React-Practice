c678358eef57437df83fd5bc3440c8d66713c6ed
Roy <156922911+ArunRoy404@users.noreply.github.com>
2025-04-19 02:22:31 +0600

    access values by use ref

--- controlForm/src/App.jsx
+++ controlForm/src/App.jsx
@@ -1,13 +1,19 @@
+import { useRef } from 'react'
 import './App.css'
 
 function App() {
 
-  const handleForm = (e) =>{
+  const handleForm = (e) => {
     e.preventDefault()
     console.log(e.target.name.value)
     e.target.name.value = ''
   }
-  
+
+  const btn2Ref = useRef(null)
+  const handleForm2 = () =>{
+    console.log(btn2Ref.current.value)
+  }
+
 
   return (
     <>
@@ -16,6 +22,12 @@ function App() {
           <input type="text" name='name' />
           <button>submit</button>
         </form>
+      </div>
+
+      <div style={{ marginTop: '30px', border: '2px solid red' }}>
+        <h3>Access value by useRef</h3>
+        <input type="text" ref={btn2Ref} />
+        <button onClick={handleForm2} >submit</button>
       </div>
     </>
   )
